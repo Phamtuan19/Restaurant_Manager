c55daecefd0954c2dd2f3c6d2f50d5de03fc0874
PhamTuan <phamtuan19hd@gmail.com>
2023-06-25 22:10:39 +0700

    .

--- front-end/src/page/client/HomePage/ContentHome/TrendingOrders/index.jsx
+++ front-end/src/page/client/HomePage/ContentHome/TrendingOrders/index.jsx
@@ -6,74 +6,74 @@ import { useContext } from 'react';
 import { ContextData } from '..';
 
 const productTrendingList = [
-    {
-        top: 'Top of the week',
-        title: 'Mushroom',
-        img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
-        alt: 'profileimage',
-        price: 120000,
-        persons: '4 persons',
-    },
-    {
-        top: 'Top of the week',
-        title: 'Mushroom',
-        img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
-        alt: 'profileimage',
-        price: 120000,
-        persons: '4 persons',
-    },
-    {
-        top: 'Top of the week',
-        title: 'Mushroom',
-        img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
-        alt: 'profileimage',
-        price: 120000,
-        persons: '4 persons',
-    },
-    {
-        top: 'Top of the week',
-        title: 'Mushroom',
-        img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
-        alt: 'profileimage',
-        price: 120000,
-        persons: '4 persons',
-    },
-    {
-        top: 'Top of the week',
-        title: 'Mushroom',
-        img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
-        alt: 'profileimage',
-        price: 120000,
-        persons: '4 persons',
-    },
-    {
-        top: 'Top of the week',
-        title: 'Mushroom',
-        img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
-        alt: 'profileimage',
-        price: 120000,
-        persons: '4 persons',
-    },
+   {
+      top: 'Top of the week',
+      name: 'Mushroom',
+      img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
+      alt: 'profileimage',
+      price: 12,
+      persons: '4 persons',
+   },
+   {
+      top: 'Top of the week',
+      name: 'Mushroom',
+      img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
+      alt: 'profileimage',
+      price: 12,
+      persons: '4 persons',
+   },
+   {
+      top: 'Top of the week',
+      name: 'Mushroom',
+      img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
+      alt: 'profileimage',
+      price: 12,
+      persons: '4 persons',
+   },
+   {
+      top: 'Top of the week',
+      name: 'Mushroom',
+      img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
+      alt: 'profileimage',
+      price: 12,
+      persons: '4 persons',
+   },
+   {
+      top: 'Top of the week',
+      name: 'Mushroom',
+      img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
+      alt: 'profileimage',
+      price: 12,
+      persons: '4 persons',
+   },
+   {
+      top: 'Top of the week',
+      name: 'Mushroom',
+      img: 'https://templates.iqonic.design/aprycot/react/build/static/media/16.2717f609.png',
+      alt: 'profileimage',
+      price: 12,
+      persons: '4 persons',
+   },
 ];
 
 function TrendingOrders() {
-    const { topProducts } = useContext(ContextData);
+   const { topProducts } = useContext(ContextData);
 
-    return (
-        <Box minHeight={500}>
-            <TitleComponent title="Trending orders" sx={{ marginTop: '12px' }} />
+   return (
+      <Box minHeight={500}>
+         <TitleComponent title="Trending orders" sx={{ marginTop: '12px' }} />
 
-            <Grid container spacing={2}>
-                {(topProducts || productTrendingList).map((item, index) => {
-                    return (
-                        <Grid item key={index} xs={12} sm={6} lg={4}>
-                            <Product data={item} />
-                        </Grid>
-                    );
-                })}
-            </Grid>
-        </Box>
-    );
+         <Grid container spacing={2}>
+            {(topProducts || productTrendingList).map((item, index) => {
+               return (
+                  <Grid item key={index} xs={12} sm={6} lg={4}>
+                     <Product data={item} />
+                  </Grid>
+               );
+            })}
+         </Grid>
+      </Box>
+   );
 }
 
 export default TrendingOrders;
